d3d720a646901385930ec4a0b41993f6961c903f
adiramardiani <adira.mardiani@gmail.com>
2025-06-06 08:40:52 +0700

    Deleted layout.tsx

--- app/(private)/tasks/_lib/constants.ts
+++
@@ -1,4 +0,0 @@
-export const cache_tag_single = 'task';
-export const cache_tag_collection = 'tasks';
-export const cache_tag_count_status = 'task-counts-status';
-export const cache_tag_count_priority = 'task-counts-priority';
--- app/(private)/tasks/model/schema.ts
+++
@@ -1,5 +0,0 @@
-import { tasks } from '@/db/schema';
-
-export type Model = typeof tasks.$inferSelect;
-
-export const modelSchema = tasks;
